6f4bb2e59a4e6ed4f862e2b1eacb5931227c0aee
Julián Miño <julian.mino@possumus.tech>
2023-08-31 20:20:16 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Julian Miño on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
